cd2f94664ba197c317ddf165934a54440511914e
Vladimir Malko <mavs.vl@gmail.com>
2018-11-30 12:38:22 +0300

    fixes

--- src/main/java/com/mavs/userservice/UserServiceApplication.java
+++ src/main/java/com/mavs/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.mavs.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCaching
 @EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
--- src/main/java/com/mavs/userservice/config/auth/RedisCacheConfig.java
+++ src/main/java/com/mavs/userservice/config/auth/RedisCacheConfig.java
@@ -1,9 +1,8 @@
 package com.mavs.userservice.config.auth;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
-@EnableCaching
+
 @Configuration
 public class RedisCacheConfig {
 }
